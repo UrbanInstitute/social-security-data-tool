173d642b338183fefcecd241923d9c2ccbde11cb
bchartoff <bchartoff@gmail.com>
2016-09-05 17:59:17 -0500

    chrome m53 dropdown bug fix

--- feature/js/urban-selects.js
+++ feature/js/urban-selects.js
@@ -1,18 +1,18 @@
 
-$(".styled-select").click(function () {
-    var element = $(this).children("select")[0],
-        worked = false;
-    if(document.createEvent) { // all browsers
-        var e = document.createEvent("MouseEvents");
-        e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,false, false, false, 0, null);
-        worked = element.dispatchEvent(e);
-    } else if (element.fireEvent) { // ie
-        worked = element.fireEvent("onmousedown");
-    }
-    if (!worked) { // unknown browser / error
-        alert("It didn't worked in your browser.");
-    }
-});
+// $(".styled-select").click(function () {
+//     var element = $(this).children("select")[0],
+//         worked = false;
+//     if(document.createEvent) { // all browsers
+//         var e = document.createEvent("MouseEvents");
+//         e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,false, false, false, 0, null);
+//         worked = element.dispatchEvent(e);
+//     } else if (element.fireEvent) { // ie
+//         worked = element.fireEvent("onmousedown");
+//     }
+//     if (!worked) { // unknown browser / error
+//         alert("It didn't worked in your browser.");
+//     }
+// });
 
 $(".styled-select select")
   .change(function(){
